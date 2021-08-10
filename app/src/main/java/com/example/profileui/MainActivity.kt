7696c7c2445669d81f21c1f1b8904035f7f77386
anshu790 <anshukumarsingh790@gmail.com>
2021-08-10 15:02:08 +0530

    This is my final UI

--- app/src/main/java/com/example/profileui/MainActivity.kt
+++ app/src/main/java/com/example/profileui/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.profileui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.example.profileui.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val states = resources.getStringArray(R.array.states)
+        val adapter = ArrayAdapter(
+                this,
+                R.layout.dropdown_item,
+                states
+        )
+        with(binding.state){
+            setAdapter(adapter)
+        }
     }
 }
